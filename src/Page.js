d34a0e1134c13226b555debc727d9919abd2f247
hemesh <hemeshreddy@gmail.com>
2018-02-26 22:36:45 +1100

    added resolution

--- src/Page.js
+++ src/Page.js
@@ -4,7 +4,7 @@ import Media from "react-media";
 class Page extends Component{
     render(){
         var stylesS = {
-            padding:'100px',
+            
             width: "100%",
             height:'60vh',
             fontFamily: "'Sacramento', cursive;",
@@ -17,11 +17,11 @@ class Page extends Component{
                   
                   <Media
                     query="(max-width: 480px)"
-                    render={() => <p style={{fontSize:'2em'}}>{this.props.contentP}</p>}
+                    render={() => <p style={{fontSize:'2em',padding:'40px'}}>{this.props.contentP}</p>}
                     />
                     <Media
                     query="(min-width: 481px)"
-                    render={() => <p style={{fontSize:'4em'}}>{this.props.contentP}</p>}
+                    render={() => <p style={{fontSize:'4em',padding:'100px'}}>{this.props.contentP}</p>}
                     />
                 </div>
         );
