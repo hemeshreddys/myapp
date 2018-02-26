d75406008dbf8d703fa9a338fe4db9497de14762
hemesh <hemeshreddy@gmail.com>
2018-02-26 20:36:13 +1100

    second commit

--- src/Page.js
+++ src/Page.js
@@ -14,7 +14,7 @@ class Page extends Component{
         };
         return(
                 <div style={stylesS}>
-                hemesh 
+                 
                 </div>
         );
     }
